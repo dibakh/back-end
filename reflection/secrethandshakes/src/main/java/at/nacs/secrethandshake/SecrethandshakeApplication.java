85afb834913d8d0697dc6aff6d44885fa87ed33c
diba <diba.kheirkhah@gmail.com>
2019-03-07 17:27:42 +0100

    main application

---
+++ reflection/secrethandshakes/src/main/java/at/nacs/secrethandshake/SecrethandshakeApplication.java
@@ -0,0 +1,13 @@
+package at.nacs.secrethandshake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecrethandshakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecrethandshakeApplication.class, args);
+    }
+
+}
