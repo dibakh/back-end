250f989148063597195ebd7d820260396b1c4a09
diba <diba.kheirkhah@gmail.com>
2019-03-15 11:34:15 +0100

    test morse code endpoint

---
+++ reflection/week4-morsecode/morse/src/test/java/at/nacs/morse/communication/MorseCodeEndpointTest.java
@@ -0,0 +1,23 @@
+package at.nacs.morse.communication;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MorseCodeEndpointTest {
+
+    private String url="/morse";
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    void postEncodedLetter() {
+        Assertions.assertThat(testRestTemplate.postForObject(url, "a", String.class)).isEqualTo("·−");
+    }
+}
