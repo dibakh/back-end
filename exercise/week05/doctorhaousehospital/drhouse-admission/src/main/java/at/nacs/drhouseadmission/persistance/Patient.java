01f297dc14369cbd8449787b8ee3b3cc4a9b1fcd
diba <diba.kheirkhah@gmail.com>
2019-03-21 16:30:45 +0100

    patient properties class

--- exercise/week05/doctorhaousehospital/drhouse-admission/src/main/java/at/nacs/drhouseadmission/persistance/Patient.java
+++ exercise/week05/doctorhaousehospital/drhouse-admission/src/main/java/at/nacs/drhouseadmission/persistance/Patient.java
@@ -1,5 +1,10 @@
-package at.nacs.drhouseadmission;
+package at.nacs.drhouseadmission.persistance;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Patient {
     private String id;
     private String name;
