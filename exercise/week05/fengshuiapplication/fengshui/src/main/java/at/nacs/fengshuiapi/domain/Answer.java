bf0ae6f83a4156d942659f8140128aff6a22b025
diba <diba.kheirkhah@gmail.com>
2019-03-25 12:42:46 +0100

    answers

---
+++ exercise/week05/fengshuiapplication/fengshui/src/main/java/at/nacs/fengshuiapi/domain/Answer.java
@@ -0,0 +1,14 @@
+package at.nacs.fengshuiapi.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Answer {
+    private String result;
+}
