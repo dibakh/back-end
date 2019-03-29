6a04ac5221e51d4fb424642f4cc24e8d31b8d73e
diba <diba.kheirkhah@gmail.com>
2019-03-29 17:24:46 +0100

    manager class

---
+++ exercise/week06/my-personal-todo-list/todos/src/main/java/at/nacs/todos/Business/ToDoManager.java
@@ -0,0 +1,49 @@
+package at.nacs.todos.Business;
+
+import at.nacs.todos.persistence.ToDo;
+import at.nacs.todos.persistence.ToDoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ToDoManager {
+
+    private final ToDoRepository repository;
+
+    public List<ToDo> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<ToDo> find(String id) {
+        return repository.findById(id);
+    }
+
+    public ToDo save(ToDo newToDo) {
+        return repository.save(newToDo);
+    }
+
+    public Optional<ToDo> markAsDone(String id) {
+        Optional<ToDo> toDo = find(id);
+        if (!toDo.isPresent()) {
+            return Optional.empty();
+        }
+
+        ToDo currentToDo = toDo.get();
+        currentToDo.setDone(true);
+        repository.save(currentToDo);
+
+        return Optional.of(currentToDo);
+    }
+
+    public void delete(String id) {
+        repository.deleteById(id);
+    }
+
+    public void deleteAll() {
+        repository.deleteAll();
+    }
+}
