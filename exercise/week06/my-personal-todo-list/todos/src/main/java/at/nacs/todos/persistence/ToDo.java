c9cb3d8de2f5dd2677d5360aa4cd0f8a67f8c958
diba <diba.kheirkhah@gmail.com>
2019-03-29 17:27:29 +0100

    attribute class

--- exercise/week06/my-personal-todo-list/todos/src/main/java/at/nacs/todos/persistence/ToDo.java
+++ exercise/week06/my-personal-todo-list/todos/src/main/java/at/nacs/todos/persistence/ToDo.java
@@ -1,8 +1,14 @@
 package at.nacs.todos.persistence;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ToDo {
     private String id;
     private String title;
