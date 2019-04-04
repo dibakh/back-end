89452bfbf6031d9c10c27dd39983e43c6ffeb56e
diba <diba.kheirkhah@gmail.com>
2019-04-04 17:50:01 +0200

    updated

--- reflection/week6-doctorhousehospital/drhouse-admission/src/main/java/at/nacs/drhouseadmission/logic/DiagnosesClient.java
+++ reflection/week6-doctorhousehospital/drhouse-admission/src/main/java/at/nacs/drhouseadmission/logic/DiagnosesClient.java
@@ -2,18 +2,17 @@ package at.nacs.drhouseadmission.logic;
 
 import at.nacs.drhouseadmission.persistance.Patient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 
-@RestController
 @RequiredArgsConstructor
 public class DiagnosesClient {
 
     private final RestTemplate restTemplate;
 
-    //    @Value("${patients.server.url}")
-    private String url = "http://localhost:9002/patients";
+    @Value("${patients.server.url}")
+    private String url;
 
     public Patient forward(Patient patient) {
         restTemplate.postForObject(url, patient, Patient.class);
