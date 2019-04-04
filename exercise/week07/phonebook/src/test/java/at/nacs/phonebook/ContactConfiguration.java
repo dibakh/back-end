5cd3f2113f90bce2143ce23bf28d48e1a55bd157
diba <diba.kheirkhah@gmail.com>
2019-04-04 09:10:15 +0200

    updated

--- exercise/week07/phonebook/src/test/java/at/nacs/phonebook/ContactConfiguration.java
+++ exercise/week07/phonebook/src/test/java/at/nacs/phonebook/ContactConfiguration.java
@@ -7,23 +7,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 @ConfigurationProperties("dataset")
 public class ContactConfiguration {
 
-//    @Setter
-//    List<Contact> contacts;
-
     @Getter
     @Setter
     Contact contact;
-
-//    @Bean
-//    List<Contact> contacts() {
-//        return contacts;
-//    }
 
     @Bean
     Contact contact() {
