639ef39aa10aa09a48c9ce7c743fdc5678a28b96
diba <diba.kheirkhah@gmail.com>
2019-04-16 22:43:12 +0200

    logic updated

--- reflection/week6-doctorhousehospital/drhouse-accountancy/src/main/java/at/nacs/drhouseaccountancy/logic/InvoiceManager.java
+++ reflection/week6-doctorhousehospital/drhouse-accountancy/src/main/java/at/nacs/drhouseaccountancy/logic/InvoiceManager.java
@@ -1,68 +1,29 @@
 package at.nacs.drhouseaccountancy.logic;
 
 import at.nacs.drhouseaccountancy.Domain.Invoice;
-import at.nacs.drhouseaccountancy.Domain.Kind;
-import at.nacs.drhouseaccountancy.Domain.Patient;
-import at.nacs.drhouseaccountancy.dto.PatientDTO;
 import at.nacs.drhouseaccountancy.persistance.InvoiceRepository;
-import at.nacs.drhouseaccountancy.persistance.PatientRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@ConfigurationProperties("price")
 public class InvoiceManager {
 
-  private final InvoiceRepository invoiceRepository;
+  private final InvoiceRepository repository;
 
-  public List<Invoice> findAll() {
-
-    return invoiceRepository.findAll();
+  public void save(Invoice invoice) {
+    repository.save(invoice);
   }
 
-  public Patient save(Invoice patientDTO) {
-    Long id = Long.valueOf(patientDTO.getId());
-    Optional<Invoice> patientInvoice = invoiceRepository.findById(id);
-    Invoice invoice = patientInvoice.get();
-    invoice.setCost(price);
-    invoiceRepository.save(invoice);
-
-    Optional<Patient> patient = patientRepository.findById(id);
-    return patient.get();
+  public List<Invoice> getAll() {
+    return repository.findAll();
   }
 
-  public void update(Long id) {
-    Invoice invoice = invoiceRepository.findById(id).orElse(null);
+  public void markAsPaidInvoice(Long id) {
+    Invoice invoice = repository.findById(id).orElse(null);
     invoice.setPaid(true);
-    invoiceRepository.save(invoice);
-  }
-
-  public Double calculateCosts(PatientDTO patientDTO) {
-
-    Double price = 0.0;
-
-    Invoice invoice = getInvoice(patientDTO);
-    String providedTreatment = patientDTO.getTreatment();
-    Kind kind = invoice.getKind();
-
-    switch (kind) {
-      case MEDICINE:
-        price = medicine.get(providedTreatment);
-        break;
-      case TREATMENT:
-        price = treathment.get(providedTreatment);
-    }
-    return price;
-  }
-
-  private Invoice getInvoice(PatientDTO patientDTO) {
-    Long patientId = Long.valueOf(patientDTO.getId());
-    return invoiceRepository.getOne(patientId);
+    repository.save(invoice);
   }
 }
