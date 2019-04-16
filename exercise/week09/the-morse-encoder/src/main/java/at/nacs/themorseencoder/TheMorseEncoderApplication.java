cd48da2ac48af1bab84ba209bbb8de18f7b91850
diba <diba.kheirkhah@gmail.com>
2019-04-16 20:08:03 +0200

    more encoder

---
+++ exercise/week09/the-morse-encoder/src/main/java/at/nacs/themorseencoder/TheMorseEncoderApplication.java
@@ -0,0 +1,13 @@
+package at.nacs.themorseencoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMorseEncoderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TheMorseEncoderApplication.class, args);
+  }
+
+}
