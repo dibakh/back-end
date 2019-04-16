4a18ca10d55031155934f873747da7f784d1aee3
diba <diba.kheirkhah@gmail.com>
2019-04-16 22:45:27 +0200

    patient attribute class

--- reflection/week6-doctorhousehospital/drhouse-accountancy/src/main/java/at/nacs/drhouseaccountancy/Domain/Patient.java
+++ reflection/week6-doctorhousehospital/drhouse-accountancy/src/main/java/at/nacs/drhouseaccountancy/Domain/Patient.java
@@ -1,6 +1,9 @@
 package at.nacs.drhouseaccountancy.Domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,6 +12,9 @@ import javax.persistence.Id;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Patient {
 
   @Id
