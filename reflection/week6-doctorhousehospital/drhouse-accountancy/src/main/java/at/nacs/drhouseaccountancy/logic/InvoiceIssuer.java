892aac729beba0da9a644d7560e55876e2542074
diba <diba.kheirkhah@gmail.com>
2019-04-16 22:36:34 +0200

    issuing a invoice

--- reflection/week6-doctorhousehospital/drhouse-accountancy/src/main/java/at/nacs/drhouseaccountancy/logic/InvoiceIssuer.java
+++ reflection/week6-doctorhousehospital/drhouse-accountancy/src/main/java/at/nacs/drhouseaccountancy/logic/InvoiceIssuer.java
@@ -1,59 +1,65 @@
 package at.nacs.drhouseaccountancy.logic;
 
 import at.nacs.drhouseaccountancy.Domain.Invoice;
-import at.nacs.drhouseaccountancy.dto.PatientDTO;
+import at.nacs.drhouseaccountancy.Domain.Kind;
+import at.nacs.drhouseaccountancy.communication.dto.PatientDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class InvoiceIssuer {
+
+  private final Map<String, Double> treatmentPrices;
+  private final Map<String, Double> medicinePrices;
+  private PatientDTO patientDTO;
+
   public Invoice generateInvoice(PatientDTO patientDTO) {
-    return null;
+    return Invoice.builder()
+                  .id(getId())
+                  .kind(getKind())
+                  .cost(getCost())
+                  .diagnosis(patientDTO.getDiagnosis())
+                  .paid(false)
+                  .provided(getProvided())
+                  .symptoms(patientDTO.getSymptoms())
+                  .timestamp(LocalDateTime.now())
+                  .build();
+  }
+
+  private Long getId() {
+    return Long.valueOf(patientDTO.getId());
+  }
+
+  private Kind getKind() {
+    if (anyMedicinePrescribed()) {
+      return Kind.MEDICINE;
+    }
+    return Kind.TREATMENT;
+  }
+
+  private double getCost() {
+    if (anyMedicinePrescribed()) {
+      return medicinePrices.get(getMedicine());
+    }
+    return treatmentPrices.get(getMedicine());
+  }
+
+  private String getProvided() {
+    if (anyMedicinePrescribed()) {
+      return patientDTO.getMedicine();
+    }
+    return patientDTO.getTreatment();
+  }
+
+  private boolean anyMedicinePrescribed() {
+    return getMedicine().isBlank();
+  }
+
+  private String getMedicine() {
+    return patientDTO.getMedicine();
   }
 }
-
-// private final PatientRepository patientRepository;
-//  private final InvoiceRepository invoiceRepository;
-//
-//  private final PatientDTO patientDTO;
-//  private Kind kind;
-//
-//  public PatientDTO saveOrUpdate(PatientDTO patientDTO) {
-//
-//    Long patientId = Long.valueOf(patientDTO.getId());
-//    Optional<Patient> patient = patientRepository.findByUuid(patientId);
-//    if (patient.isPresent()) {
-//      generateInvoice(patientDTO, patient.get());
-//    }
-//    patientRepository.save(patient.get());
-//  }
-//
-//  private void generateInvoice(PatientDTO patientDTO, Patient patient) {
-//    Invoice invoice = Invoice.builder()
-//                             .patient(patient)
-//                             .symptoms(patientDTO.getSymptoms())
-//                             .diagnosis(patientDTO.getDiagnosis())
-//                             .kind(getKindOfInvoice())
-//                             .provided(getProvided())
-//                             .cost(calculatesCosts())
-//                             .paid(false)
-//                             .timestamp(LocalDateTime.now())
-//                             .build();
-//
-//    invoiceRepository.save(invoice);
-//  }
-//
-//  private double calculatesCosts() {
-//    // data from yml file
-//    return 0;
-//  }
-//
-//  private Kind getKindOfInvoice() {
-//    if (patientDTO.getMedicine().isEmpty()) {
-//      kind = Kind.TREATMENT;
-//    }
-//    kind = Kind.MEDICINE;
-//
-//    return kind;
-//  }
-//
-//  private String getProvided() {
-//    return null;
-//  }
