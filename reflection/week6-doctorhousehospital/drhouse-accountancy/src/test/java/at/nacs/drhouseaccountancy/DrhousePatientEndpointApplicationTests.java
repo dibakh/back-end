727c2fdde3b69052fb078dd56bae5331020c5e12
diba <diba.kheirkhah@gmail.com>
2019-04-17 08:00:29 +0200

    updated

--- reflection/week6-doctorhousehospital/drhouse-accountancy/src/test/java/at/nacs/drhouseaccountancy/DrhousePatientEndpointApplicationTests.java
+++ reflection/week6-doctorhousehospital/drhouse-accountancy/src/test/java/at/nacs/drhouseaccountancy/DrhousePatientEndpointApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DrhouseAccountancyEndpointApplicationTests {
+public class DrhousePatientEndpointApplicationTests {
 
     @Test
     public void contextLoads() {
