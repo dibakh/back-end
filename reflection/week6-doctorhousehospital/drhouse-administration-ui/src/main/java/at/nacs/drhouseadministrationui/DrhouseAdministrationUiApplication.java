f1c9a78f03a1541e6ec855f3742d9ad19077195e
diba <diba.kheirkhah@gmail.com>
2019-04-18 15:39:08 +0200

    ui added

---
+++ reflection/week6-doctorhousehospital/drhouse-administration-ui/src/main/java/at/nacs/drhouseadministrationui/DrhouseAdministrationUiApplication.java
@@ -0,0 +1,13 @@
+package at.nacs.drhouseadministrationui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrhouseAdministrationUiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DrhouseAdministrationUiApplication.class, args);
+  }
+
+}
