a7a03675a4d772cfab4d4b59a1364a9f293157a5
diba <diba.kheirkhah@gmail.com>
2019-04-18 15:36:59 +0200

    submittion controller

---
+++ reflection/week6-doctorhousehospital/drhouse-administration-ui/src/main/java/at/nacs/drhouseadministrationui/view/controller/SubmittionController.java
@@ -0,0 +1,40 @@
+package at.nacs.drhouseadministrationui.view.controller;
+
+import at.nacs.drhouseadministrationui.communication.PatientClient;
+import at.nacs.drhouseadministrationui.view.model.Patient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class SubmittionController {
+
+  private final PatientClient client;
+
+  @ModelAttribute("patient")
+  Patient patient() {
+    return new Patient();
+  }
+
+  @GetMapping
+  String page() {
+    return "patient-submission";
+  }
+
+  @PostMapping
+  String post(@Valid Patient patient, BindingResult result) {
+    if (result.hasErrors()) {
+      return page();
+    }
+    client.save(patient);
+    return "redirect:/";
+  }
+}
