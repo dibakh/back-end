a7b691cdc2b9ff660746471b6fcb0277b13cea99
diba <diba.kheirkhah@gmail.com>
2019-04-23 20:22:59 +0200

    list of treasures

---
+++ exercise/week10/the-thieves-den/week10the-thieves-den/src/main/java/at/nacs/thethievesden/communication/TreasuresEndpoint.java
@@ -0,0 +1,17 @@
+package at.nacs.thethievesden.communication;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TreasuresEndpoint {
+
+  @GetMapping
+  String message() {
+    return "-------------- LIST OF TREASURES -----------";
+  }
+
+}
--- reflection/week6-doctorhousehospital/drhouse-admission/src/main/java/at/nacs/drhouseadmission/communication/DiagnosesClient.java
+++ reflection/week6-doctorhousehospital/drhouse-admission/src/main/java/at/nacs/drhouseadmission/communication/DiagnosesClient.java
@@ -3,9 +3,11 @@ package at.nacs.drhouseadmission.logic;
 import at.nacs.drhouseadmission.persistance.Patient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-
+@Service
 @RequiredArgsConstructor
 public class DiagnosesClient {
 
