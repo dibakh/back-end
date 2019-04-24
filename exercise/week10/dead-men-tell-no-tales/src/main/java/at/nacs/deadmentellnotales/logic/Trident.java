078699f190234ba1befd50f8aebe3e03de326632
diba <diba.kheirkhah@gmail.com>
2019-04-24 11:23:26 +0200

    trident class

---
+++ exercise/week10/dead-men-tell-no-tales/src/main/java/at/nacs/deadmentellnotales/logic/Trident.java
@@ -0,0 +1,13 @@
+package at.nacs.deadmentellnotales.logic;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Trident {
+
+  @Getter
+  @Setter
+  private boolean status;
+}
