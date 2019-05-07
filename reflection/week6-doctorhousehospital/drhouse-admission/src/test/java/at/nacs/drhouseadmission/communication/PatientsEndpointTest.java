8f5630bffac4fb680f3182dba0eb1e98490f76f1
diba <diba.kheirkhah@gmail.com>
2019-05-07 05:51:29 +0200

    formatted

--- reflection/week6-doctorhousehospital/drhouse-admission/src/test/java/at/nacs/drhouseadmission/communication/PatientsEndpointTest.java
+++ reflection/week6-doctorhousehospital/drhouse-admission/src/test/java/at/nacs/drhouseadmission/communication/PatientsEndpointTest.java
@@ -3,34 +3,31 @@ package at.nacs.drhouseadmission.communication;
 import at.nacs.drhouseadmission.logic.Admission;
 import at.nacs.drhouseadmission.persistance.Patient;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.web.client.RestTemplate;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class PatientsEndpointTest {
 
-    @Autowired
-    TestRestTemplate testRestTemplate;
+  @Autowired
+  TestRestTemplate testRestTemplate;
 
-    @MockBean
-    DiagnosesClient client;
+  @MockBean
+  DiagnosesClient client;
 
-    @SpyBean
-    Admission  admission;
+  @SpyBean
+  Admission admission;
 
-    @Autowired
-    Patient patient;
+  @Autowired
+  Patient patient;
 
-    @Test
-    void postPatient() {
+  @Test
+  void postPatient() {
 //        Mockito.when(diagnosesClient.forward(patient))
 //                .thenReturn(patient);
 //
@@ -40,5 +37,5 @@ class PatientsEndpointTest {
 //        assertThat(actual.getName()).isEqualTo(patient.getName());
 //        assertThat(actual.getSymptoms()).isEqualTo(patient.getSymptoms());
 //        assertThat(actual.getId()).isNotEmpty();
-    }
+  }
 }
