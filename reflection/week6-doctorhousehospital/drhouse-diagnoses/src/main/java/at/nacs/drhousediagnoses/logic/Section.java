5067e9b118ef4abb7f0e79b9cfdc0db69c6e9584
diba <diba.kheirkhah@gmail.com>
2019-05-07 21:08:02 +0200

    add logic

---
+++ reflection/week6-doctorhousehospital/drhouse-diagnoses/src/main/java/at/nacs/drhousediagnoses/logic/Section.java
@@ -0,0 +1,9 @@
+package at.nacs.drhousediagnoses.logic;
+
+import at.nacs.drhousediagnoses.persistance.Patient;
+
+public class Section {
+  public Patient refer(Patient patient){
+    return patient;
+  }
+}
