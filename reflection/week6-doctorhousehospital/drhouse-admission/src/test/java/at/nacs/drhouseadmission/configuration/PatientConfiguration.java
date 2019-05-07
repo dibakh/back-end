eba62e8f672b811193999e4fbb5d5b10833eddd2
diba <diba.kheirkhah@gmail.com>
2019-05-07 05:46:45 +0200

    add getter and setter

--- reflection/week6-doctorhousehospital/drhouse-admission/src/test/java/at/nacs/drhouseadmission/configuration/PatientConfiguration.java
+++ reflection/week6-doctorhousehospital/drhouse-admission/src/test/java/at/nacs/drhouseadmission/configuration/PatientConfiguration.java
@@ -1,21 +1,22 @@
 package at.nacs.drhouseadmission.configuration;
 
 import at.nacs.drhouseadmission.persistance.Patient;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties("dataset")
 public class PatientConfiguration {
 
-    @Bean
-    Patient getPatient() {
-        return Patient.builder()
-                .name("name")
-                .symptoms("symptoms")
-                .build();
-    }
+  @Getter
+  @Setter
+  Patient patient;
 
-    ;
-
-
+  @Bean
+  Patient getPatient() {
+    return patient;
+  }
 }
