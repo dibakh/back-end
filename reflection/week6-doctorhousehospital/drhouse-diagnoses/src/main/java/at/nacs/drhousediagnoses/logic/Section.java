253a0d90ac8c7a2c725292ae00ec6b4bbecc2313
diba <diba.kheirkhah@gmail.com>
2019-05-07 21:22:50 +0200

    change logic

--- reflection/week6-doctorhousehospital/drhouse-diagnoses/src/main/java/at/nacs/drhousediagnoses/logic/Section.java
+++ reflection/week6-doctorhousehospital/drhouse-diagnoses/src/main/java/at/nacs/drhousediagnoses/logic/Section.java
@@ -1,9 +1,32 @@
 package at.nacs.drhousediagnoses.logic;
 
+import at.nacs.drhousediagnoses.communication.BedClient;
+import at.nacs.drhousediagnoses.communication.PharmacyClient;
 import at.nacs.drhousediagnoses.persistance.Patient;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@ConfigurationProperties("patient")
 public class Section {
+
+  @Setter
+  private Map<String, String> treatment;
+
+  private final PharmacyClient pharmacyClient;
+  private final BedClient bedClient;
+
   public Patient refer(Patient patient){
+    String diagnosis = patient.getDiagnosis();
+    String section = treatment.get(diagnosis);
+
+
+
     return patient;
   }
 }
