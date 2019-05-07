0f96264c5ac1631b1a0c05fd33c6f70e5cd3f6a5
diba <diba.kheirkhah@gmail.com>
2019-05-07 20:37:15 +0200

    edit annotations

--- reflection/week6-doctorhousehospital/drhouse-diagnoses/src/main/java/at/nacs/drhousediagnoses/persistance/Patient.java
+++ reflection/week6-doctorhousehospital/drhouse-diagnoses/src/main/java/at/nacs/drhousediagnoses/persistance/Patient.java
@@ -1,18 +1,12 @@
 package at.nacs.drhousediagnoses.persistance;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Patient {
-    private String id;
-    private String name;
-    private String symptoms;
-    private String diagnosis;
+  private String id;
+  private String name;
+  private String symptoms;
+  private String diagnosis;
 
 }
