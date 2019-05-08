97c42aa8275cd705666ace7fc8fba51abd9b50eb
diba <diba.kheirkhah@gmail.com>
2019-05-08 11:52:33 +0200

    refer patient to bed or pharmacy

--- reflection/week6-doctorhousehospital/drhouse-diagnoses/src/main/java/at/nacs/drhousediagnoses/logic/DoctorHouseAssistant.java
+++ reflection/week6-doctorhousehospital/drhouse-diagnoses/src/main/java/at/nacs/drhousediagnoses/logic/DoctorHouseAssistant.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 @ConfigurationProperties("patient")
-public class Section {
+public class DoctorHouseAssistant {
 
   @Setter
   private Map<String, String> treatment;
@@ -21,12 +21,12 @@ public class Section {
   private final PharmacyClient pharmacyClient;
   private final BedClient bedClient;
 
-  public Patient refer(Patient patient){
+  public void refer(Patient patient){
     String diagnosis = patient.getDiagnosis();
     String section = treatment.get(diagnosis);
-
-
-
-    return patient;
+    if (section.equals("bed")){
+      bedClient.forward(patient);
+    }
+    pharmacyClient.forward(patient);
   }
 }
