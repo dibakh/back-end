0bcdbf6260d15266fbaa63c96db0982bd4138295
diba <diba.kheirkhah@gmail.com>
2019-05-22 13:02:03 +0200

    update cashier

---
+++ project/coding-cafe/cashier/src/test/java/at/nacs/cashier/CashierEndpointTest.java
@@ -0,0 +1,30 @@
+package at.nacs.cashier;
+
+import at.nacs.cashier.persistance.domain.BaristaClient;
+import at.nacs.cashier.persistance.domain.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class CashierEndpointTest {
+
+  @Autowired
+  TestRestTemplate restTemplate;
+
+  @MockBean
+  BaristaClient baristaClient;
+
+  String url = "/barista";
+
+  @Test
+  void get() {
+    restTemplate.postForObject(url, Order.class);
+    verify(messagesClient).getMessage();
+  }
+}
